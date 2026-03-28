95c9664b011e2e3995386fa49d86fbae82c6e432
Eliseo-Baroni-97 <elibaroni97@gmail.com>
2026-03-28 05:03:18 -0300

    Guardar cambios recientes

--- src/data/careers/fotografia.ts
+++ src/data/careers/fotografia.ts
@@ -8,28 +8,7 @@ export const fotografia: CareerMap = {
     priorYearsApproved: false,
   },
   nodes: [
-    {
-      id: "laboratorio1",
-      name: "Laboratorio I",
-      year: 1,
-      correlativasCursar: [],
-      correlativasAprobar: [],
-      cuatrimestre: 1,
-      duration: "PRIMER_SEMESTRE",
-    },
-    {
-      id: "laboratorio2",
-      name: "Laboratorio II",
-      year: 1,
-      correlativasCursar: [
-        { subjectId: "laboratorio1", requiredState: "APROBADA" },
-      ],
-      correlativasAprobar: [
-        { subjectId: "laboratorio1", requiredState: "APROBADA" },
-      ],
-      cuatrimestre: 2,
-      duration: "SEGUNDO_SEMESTRE",
-    },
+    // 1° AÑO
     {
       id: "fotografia1",
       name: "Fotografía I",
@@ -37,34 +16,100 @@ export const fotografia: CareerMap = {
       correlativasCursar: [],
       correlativasAprobar: [],
       cuatrimestre: 1,
-      duration: "PRIMER_SEMESTRE",
+      duration: "ANUAL",
     },
     {
-      id: "fotografia2",
-      name: "Fotografía II",
-      year: 1,
-      correlativasCursar: [
-        { subjectId: "fotografia1", requiredState: "APROBADA" },
-      ],
-      correlativasAprobar: [
-        { subjectId: "fotografia1", requiredState: "APROBADA" },
-      ],
-      cuatrimestre: 2,
-      duration: "SEGUNDO_SEMESTRE",
-    },
-    {
-      id: "practica",
-      name: "Práctica Fotográfica",
+      id: "laboratorio1",
+      name: "Laboratorio I",
       year: 1,
       correlativasCursar: [],
       correlativasAprobar: [],
       cuatrimestre: 1,
-      duration: "PRIMER_SEMESTRE",
+      duration: "ANUAL",
     },
     {
-      id: "fotografia_artistica",
-      name: "Fotografía Artística",
+      id: "historia_fotografia",
+      name: "Historia de la Fotografía",
+      year: 1,
+      correlativasCursar: [],
+      correlativasAprobar: [],
+      cuatrimestre: 1,
+      duration: "ANUAL",
+    },
+    {
+      id: "ingles",
+      name: "Inglés",
+      year: 1,
+      correlativasCursar: [],
+      correlativasAprobar: [],
+      cuatrimestre: 1,
+      duration: "ANUAL",
+    },
+
+    // 2° AÑO
+    {
+      id: "fotografia2",
+      name: "Fotografía II",
       year: 2,
+      correlativasCursar: [
+        { subjectId: "fotografia1", requiredState: "REGULAR" },
+        { subjectId: "laboratorio1", requiredState: "REGULAR" },
+      ],
+      correlativasAprobar: [
+        { subjectId: "fotografia1", requiredState: "APROBADA" },
+        { subjectId: "laboratorio1", requiredState: "APROBADA" },
+      ],
+      cuatrimestre: 2,
+      duration: "ANUAL",
+    },
+    {
+      id: "laboratorio2",
+      name: "Laboratorio II",
+      year: 2,
+      correlativasCursar: [
+        { subjectId: "fotografia1", requiredState: "REGULAR" },
+        { subjectId: "laboratorio1", requiredState: "REGULAR" },
+      ],
+      correlativasAprobar: [
+        { subjectId: "fotografia1", requiredState: "APROBADA" },
+        { subjectId: "laboratorio1", requiredState: "APROBADA" },
+      ],
+      cuatrimestre: 2,
+      duration: "ANUAL",
+    },
+    {
+      id: "quimica",
+      name: "Química",
+      year: 2,
+      correlativasCursar: [],
+      correlativasAprobar: [],
+      cuatrimestre: 2,
+      duration: "ANUAL",
+    },
+
+    // 3° AÑO
+    {
+      id: "fotografia_color",
+      name: "Fotografía Color",
+      year: 3,
+      correlativasCursar: [
+        { subjectId: "laboratorio1", requiredState: "APROBADA" },
+        { subjectId: "laboratorio2", requiredState: "REGULAR" },
+        { subjectId: "fotografia1", requiredState: "APROBADA" },
+        { subjectId: "fotografia2", requiredState: "REGULAR" },
+        { subjectId: "quimica", requiredState: "REGULAR" },
+      ],
+      correlativasAprobar: [
+        { subjectId: "laboratorio2", requiredState: "APROBADA" },
+        { subjectId: "fotografia2", requiredState: "APROBADA" },
+      ],
+      cuatrimestre: 3,
+      duration: "ANUAL",
+    },
+    {
+      id: "practica_fotografica",
+      name: "Práctica Fotográfica",
+      year: 3,
       correlativasCursar: [
         { subjectId: "laboratorio1", requiredState: "APROBADA" },
         { subjectId: "laboratorio2", requiredState: "REGULAR" },
@@ -76,12 +121,29 @@ export const fotografia: CareerMap = {
         { subjectId: "fotografia2", requiredState: "APROBADA" },
       ],
       cuatrimestre: 3,
-      duration: "PRIMER_SEMESTRE",
+      duration: "ANUAL",
+    },
+    {
+      id: "fotografia_artistica",
+      name: "Fotografía Artística",
+      year: 3,
+      correlativasCursar: [
+        { subjectId: "laboratorio1", requiredState: "APROBADA" },
+        { subjectId: "laboratorio2", requiredState: "REGULAR" },
+        { subjectId: "fotografia1", requiredState: "APROBADA" },
+        { subjectId: "fotografia2", requiredState: "REGULAR" },
+      ],
+      correlativasAprobar: [
+        { subjectId: "laboratorio2", requiredState: "APROBADA" },
+        { subjectId: "fotografia2", requiredState: "APROBADA" },
+      ],
+      cuatrimestre: 3,
+      duration: "ANUAL",
     },
     {
       id: "fotografia_publicitaria",
       name: "Fotografía Publicitaria",
-      year: 2,
+      year: 3,
       correlativasCursar: [
         { subjectId: "laboratorio1", requiredState: "APROBADA" },
         { subjectId: "laboratorio2", requiredState: "REGULAR" },
@@ -92,13 +154,13 @@ export const fotografia: CareerMap = {
         { subjectId: "laboratorio2", requiredState: "APROBADA" },
         { subjectId: "fotografia2", requiredState: "APROBADA" },
       ],
-      cuatrimestre: 4,
-      duration: "SEGUNDO_SEMESTRE",
+      cuatrimestre: 3,
+      duration: "ANUAL",
     },
     {
       id: "medios_audiovisuales",
       name: "Medios Audiovisuales",
-      year: 2,
+      year: 3,
       correlativasCursar: [
         { subjectId: "laboratorio1", requiredState: "APROBADA" },
         { subjectId: "laboratorio2", requiredState: "REGULAR" },
@@ -109,8 +171,8 @@ export const fotografia: CareerMap = {
         { subjectId: "laboratorio2", requiredState: "APROBADA" },
         { subjectId: "fotografia2", requiredState: "APROBADA" },
       ],
-      cuatrimestre: 5,
-      duration: "PRIMER_SEMESTRE",
+      cuatrimestre: 3,
+      duration: "ANUAL",
     },
   ],
 };
